3791edcaecd27550bd523adfe8efe832b1f082ce
Jordan Jay <jordanjay28@gmail.com>
2019-02-17 15:29:36 -0800

    Include button for sorting, move on to SVG example

--- visualization/main.js
+++ visualization/main.js
@@ -5,10 +5,37 @@ d3.csv('http://localhost:3000/career_passing_stats').then(function(data) {
 		.data(data)
 		.enter()
 		.append('div')
+		.attr('class', 'bar')
 		.style('width', function(d) {
 			return d.TD + 'px';
 		})
 		.text(function(d) {
 			return d.Player + ' ' + d.TD;
 		});
+
+	d3.selectAll('.bar').on('click', function() {
+		console.log('The bar graph has been clicked');
+	});
+});
+
+document.getElementById('sort').addEventListener('click', function() {
+	let data = d3.select('.chart').selectAll('div').data();
+	const sortedData = data.sort(function(x, y) {
+		return y.TD - x.TD;
+	});
+
+	// This is going to remove all of the data before entering new data
+	let bars = d3.select('.chart').selectAll('div').remove().exit().data(sortedData);
+	bars
+		.enter()
+		.append('div')
+		.attr('class', 'bar')
+		.style('width', function(d) {
+			return d.TD + 'px';
+		})
+		.text(function(d) {
+			return d.Player + ' ' + d.TD;
+		});
+
+	console.log('The value of the sorted data is:', sortedData);
 });
