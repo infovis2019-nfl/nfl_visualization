75bef1f3e320c99afd3dd6b35299a60671d3b3d5
Jordan Jay <jordanjay28@gmail.com>
2019-02-17 15:35:09 -0800

    Include basic node server

---
+++ nfl_server/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.get('/', function(req, res) {
+	res.send('Hit the home page of the server');
+});
+
+app.get('/career_passing_stats', function(req, res) {
+	res.sendFile(__dirname + '/data/career_passing_stats.csv', function(err) {
+		if (!err) console.log('File has been sent');
+	});
+});
+
+app.listen(process.env.PORT, function() {
+	console.log('NFL Server listening on PORT:', process.env.PORT);
+});
