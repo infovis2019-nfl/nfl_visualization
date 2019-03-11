311ab0e0efdf1dd7476bb04b67f30edd40c8a0cd
Jordan Jay <jordanjay28@gmail.com>
2019-03-11 00:26:17 -0700

    Remove logging and comments

--- visualization/normalize.js
+++ visualization/normalize.js
@@ -1,35 +1,15 @@
-/*
-Steps to normalize:
-1. Find the min and max values
-2. Normalize
-*/
-
 // The data and attributes that we should be normalizing
 const normalizeSelectedAttributes = (data, attributes) => {
-	console.log('The normalize function was called');
 	for (let i = 0; i < attributes.length; i++) {
 		// first find the max values of each
 		const values = data.map((player) => player[attributes[i]]);
-		// const max = Math.max((...data)[attributes[i]])
 		const max = Math.max(...values);
 		const min = Math.min(...values);
 
-		console.log(`The max value for ${attributes[i]} is: ${max}`);
-		console.log(`The min value for ${attributes[i]} is: ${min}`);
-		console.log('The value of data is:', data);
-		// data[attributes[i] + '-Normalized'] =
+		// normalize with respect to the max and min values
 		for (let j = 0; j < data.length; j++) {
 			const normalizedValue = (data[j][attributes[i]] - min) / (max - min);
 			data[j][attributes[i] + '-Normalized'] = normalizedValue;
 		}
-	}
-	console.log('The new value of data is:', data);
-	for (let i = 0; i < data.length; i++) {
-		const player = data[i];
-		console.log(
-			`${player.Player}: ${player['Cmp%-Normalized']} ${player['Yds-Normalized']} ${player[
-				'TD-Normalized'
-			]} ${player['Rate-Normalized']} ${player['W-Normalized']} `
-		);
 	}
 };
