9106bd89cff8f7289e6cb3bdd5cd46cd101ff258
Jordan Jay <jordanjay28@gmail.com>
2019-03-24 02:40:30 -0700

    Increase height of SVG to prevent shifting

--- visualization/scatter_plot.js
+++ visualization/scatter_plot.js
@@ -1,6 +1,6 @@
 const margin = { top: 20, right: 20, bottom: 50, left: 50 },
 	width = 960 - margin.left - margin.right,
-	height = 500 - margin.top - margin.bottom;
+	height = 600 - margin.top - margin.bottom;
 
 /* 
  * value accessor - returns the value to encode for a given data object.
